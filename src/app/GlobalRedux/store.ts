262554e43ec7b45cc96154951f2104f4cf03033f
Yemesrach24 <99786617+Yemesrach24@users.noreply.github.com>
2025-01-12 23:08:53 +0300

    Update store.ts

--- src/app/GlobalRedux/store.ts
+++ src/app/GlobalRedux/store.ts
@@ -1,13 +1,12 @@
-'use client';
-
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from './Features/counter/counterSlice';
+// src/GlobalRedux/store.ts
+import { configureStore } from "@reduxjs/toolkit";
+import firebaseReducer from "./Features/firebaseSlice";
 
 export const store = configureStore({
-    reducer: {
-        counter: counterReducer
-    }
-})
+  reducer: {
+    firebase: firebaseReducer,
+  },
+});
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
